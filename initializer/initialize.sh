f36356d05ab4bb14f97ba25ef221ddf759ac49db
John Wass <jwass3@gmail.com>
2016-04-22 10:02:38 -0400

    cleanup init script; add defaults

--- initializer/initialize.sh
+++ initializer/initialize.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-export ESHOST=localhost
-export INDEX="us_large_cities"
+export ES_HOST="${ES_HOST:-elastic}"
+export ES_INDEX="${ES_INDEX:-us_large_cities}"
 
-./create_index.sh "$ESHOST" "$INDEX"
-./insert_data.sh "$ESHOST" "$INDEX"
+$(create_index.sh "$ES_HOST" "$ES_INDEX")
+$(insert_data.sh "$ES_HOST" "$ES_INDEX")
 
 echo "Testing data;  results should be [San Francisco, Berkeley, Oakland, Richmond]"
-./test_data.sh "$ESHOST" "$INDEX"
+$(test_data.sh "$ES_HOST" "$ES_INDEX")
