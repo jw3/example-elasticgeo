090c80e0670d0a5bc2cdfab6d0adfeec079394bf
John Wass <jwass3@gmail.com>
2016-04-22 10:01:26 -0400

    move test query out to json file

--- initializer/test_data.sh
+++ initializer/test_data.sh
@@ -3,27 +3,8 @@
 testdata() {
     declare eshost="$1" esidx="$2"
 
-    local res=$(curl -s -XGET "http://$eshost:9200/$esidx/city/_search?pretty=true" -d '
-    {
-      "query": {
-        "filtered" : {
-            "query" : {
-                "match_all" : {}
-            },
-            "filter" : {
-                "geo_distance" : {
-                    "distance" : "20km",
-                    "location" : {
-                        "lat" : 37.9174,
-                        "lon" : -122.3050
-                    }
-                }
-            }
-        }
-      }
-    }')
-
-    local ret=$(echo "$res" | jq -rc [.hits.hits[]._source.city])
+    local res=$(curl -s -XGET -d @test.json "http://$eshost:9200/$esidx/city/_search")
+    local ret=$(echo "$res" | jq -c [.hits.hits[]._source.city])
     echo "$ret"
 }
 
